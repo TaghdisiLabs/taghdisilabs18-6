b9aa9a840f1a41db0febdcca62d12f5a3791d743
TaghdisiLabs <mahdi.taghdisi@icloud.com>
2026-06-18 04:55:16 +0330

    Update CharacterSystem.tsx

--- src/components/characters/CharacterSystem.tsx
+++ src/components/characters/CharacterSystem.tsx
@@ -9,28 +9,28 @@ export default function CharacterSystem() {
       <motion.img
         src="/scoutT.png"
         className="w-20 absolute left-10 top-24"
-        animate={{ y: [0, -20, 0] }}
+        animate={{ y: [0, -25, 0] }}
         transition={{ duration: 4, repeat: Infinity }}
       />
 
       <motion.img
         src="/ninjaT.png"
         className="w-20 absolute right-10 top-40"
-        animate={{ scale: [1, 1.1, 1] }}
+        animate={{ scale: [1, 1.15, 1] }}
         transition={{ duration: 2.5, repeat: Infinity }}
       />
 
       <motion.img
         src="/builderT.png"
         className="w-24 absolute left-1/3 bottom-20"
-        animate={{ y: [0, -15, 0] }}
-        transition={{ duration: 3, repeat: Infinity }}
+        animate={{ rotate: [0, 1, -1, 0] }}
+        transition={{ duration: 6, repeat: Infinity }}
       />
 
       <motion.img
         src="/captainT.png"
         className="w-24 absolute right-1/3 bottom-28"
-        animate={{ rotate: [0, 2, -2, 0] }}
+        animate={{ y: [0, -10, 0] }}
         transition={{ duration: 5, repeat: Infinity }}
       />
 
