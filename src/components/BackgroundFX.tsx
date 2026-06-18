854dcbd0830a76fa83f45e41698efd655ae83b7b
TaghdisiLabs <mahdi.taghdisi@icloud.com>
2026-06-18 04:46:13 +0330

    Update BackgroundFX.tsx

--- src/components/BackgroundFX.tsx
+++ src/components/BackgroundFX.tsx
@@ -2,20 +2,14 @@
 
 export default function BackgroundFX() {
   return (
-    <div className="absolute inset-0">
+    <div className="absolute inset-0 overflow-hidden">
 
-      {/* Gradient Core */}
-      <div className="absolute inset-0 bg-gradient-to-br from-black via-[#050816] to-black" />
+      <div className="absolute inset-0 bg-black" />
 
-      {/* Glow Orbs */}
-      <div className="absolute top-20 left-20 w-72 h-72 bg-blue-500/20 blur-3xl rounded-full" />
-      <div className="absolute bottom-20 right-20 w-72 h-72 bg-cyan-400/20 blur-3xl rounded-full" />
+      <div className="absolute top-[-200px] left-[-200px] w-[600px] h-[600px] bg-blue-500/20 blur-[120px] rounded-full" />
+      <div className="absolute bottom-[-200px] right-[-200px] w-[600px] h-[600px] bg-cyan-400/20 blur-[120px] rounded-full" />
 
-      {/* Grid */}
-      <div className="absolute inset-0 opacity-10 
-        bg-[linear-gradient(to_right,#ffffff10_1px,transparent_1px),
-        linear-gradient(to_bottom,#ffffff10_1px,transparent_1px)]
-        bg-[size:40px_40px]" />
+      <div className="absolute inset-0 opacity-10 bg-[radial-gradient(circle,#ffffff10_1px,transparent_1px)] bg-[size:30px_30px]" />
 
     </div>
   );
