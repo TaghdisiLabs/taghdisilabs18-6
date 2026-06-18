224409c3de28f1696c1866d15453e2f37cd13d14
TaghdisiLabs <mahdi.taghdisi@icloud.com>
2026-06-18 04:54:05 +0330

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,11 @@
 import Hero from "@/components/Hero";
 import Services from "@/components/Services";
-import Portfolio from "@/components/Portfolio";
-import Pricing from "@/components/Pricing";
-import CTA from "@/components/CTA";
-import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <main className="relative bg-black text-white overflow-hidden">
+    <main className="bg-black text-white overflow-hidden">
       <Hero />
       <Services />
-      <Portfolio />
-      <Pricing />
-      <CTA />
-      <Footer />
     </main>
   );
 }
