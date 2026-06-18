8ac1c5c5907c4fd8a7dfeb6513432f03a44035b6
TaghdisiLabs <mahdi.taghdisi@icloud.com>
2026-06-18 05:27:15 +0330

    Update Container.tsx

--- src/components/ui/Container.tsx
+++ src/components/ui/Container.tsx
@@ -1,15 +1,10 @@
-import { ReactNode } from "react";
-import { cn } from "@/lib/utils";
-
 export default function Container({
   children,
-  className,
 }: {
-  children: ReactNode;
-  className?: string;
+  children: React.ReactNode;
 }) {
   return (
-    <div className={cn("max-w-[1200px] mx-auto px-4", className)}>
+    <div className="max-w-6xl mx-auto px-4">
       {children}
     </div>
   );
