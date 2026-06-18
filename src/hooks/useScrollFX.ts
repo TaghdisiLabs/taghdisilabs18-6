ea9d8afa8092dcd24fd0858da4c504ab2ba6ba72
TaghdisiLabs <mahdi.taghdisi@icloud.com>
2026-06-18 04:57:56 +0330

    Create useScrollFX.ts

---
+++ src/hooks/useScrollFX.ts
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+
+export function useScrollFX() {
+  const [scrollY, setScrollY] = useState(0);
+
+  useEffect(() => {
+    const onScroll = () => setScrollY(window.scrollY);
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  return scrollY;
+}
