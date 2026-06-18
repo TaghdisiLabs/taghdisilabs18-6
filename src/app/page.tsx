de434d9476914fcd82b9a2821abb93dc702708b7
TaghdisiLabs <mahdi.taghdisi@icloud.com>
2026-06-18 05:58:22 +0330

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,21 @@ import Hero from "@/components/hero/Hero";
 import Services from "@/components/services/Services";
 import Portfolio from "@/components/portfolio/Portfolio";
 import CTA from "@/components/cta/CTA";
-import BackgroundFX from "@/components/background/BackgroundFX";
 import MiniTZ from "@/components/mini-tz/MiniTZ";
+import BackgroundFX from "@/components/background/BackgroundFX";
 
 export default function Home() {
   return (
-    <main className="relative text-white">
+    <main className="relative overflow-hidden">
       <BackgroundFX />
 
       <Hero />
-      <Services />
-      <Portfolio />
-      <CTA />
+
+      <div className="space-y-32 py-24">
+        <Services />
+        <Portfolio />
+        <CTA />
+      </div>
 
       <MiniTZ />
     </main>
